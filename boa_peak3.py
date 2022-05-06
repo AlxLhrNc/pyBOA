2f7b7d332f9a216ec26aab3ec852a1772ae52c53
AlxndrLhr <90059220+AlxndrLhr@users.noreply.github.com>
2022-05-06 14:21:17 +1200

    Add files via upload
    Adding the peak3, peak5 and contextual filtering files.

---
+++ boa_peak3.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 08:37:25 2022
+
+@author: alhe551
+"""
+from numpy import nanmax, nanmin, zeros_like
+
+def peak_3(data_nc):
+    '''
+    Detection of extrema in 3*3 windows
+    Input:
+        data_nc:   .nc file
+    Output:
+        peak3:     data sheet with data_nc dimensions
+    '''
+    nc_shape = data_nc.shape
+    range_lat = range(2,nc_shape[0]-2,1)
+    range_lon = range(2,nc_shape[1]-2,1)
+    peak3 = zeros_like(data_nc)
+    
+    for i in range_lat:
+        for j in range_lon:
+            # window
+            window = data_nc[i-1:i+2,j-1:j+2]
+
+            #center
+            center = window[1,1]
+
+            peak_min = center == nanmin(window)
+            peak_max = center == nanmax(window)
+
+            if peak_min | peak_max:
+                peak3[i,j] = 1
+
+    return peak3
---
+++ boa_peak5.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 08:37:25 2022
+
+@author: alhe551
+"""
+
+from numpy import nanmax, nanmin, zeros_like
+
+def peak_5(data_nc):
+    '''
+    Detection of extrema in 5*5 windows
+    Input:
+        data_nc:   .nc file
+    Output:
+        peak5:     data sheet with data_nc dimensions
+    '''
+    nc_shape = data_nc.shape
+    range_lat = range(2,nc_shape[0]-2,1)
+    range_lon = range(2,nc_shape[1]-2,1)
+    peak5 = zeros_like(data_nc)
+
+    for i in range_lat:
+        for j in range_lon:
+            # window
+            window = data_nc[i-2:i+3,j-2:j+3]
+
+            #center
+            center = window[2,2]
+
+            # W-E
+            slice_w_e = window[2,:]
+            # N-S
+            slice_n_s = window[:,2]
+            # NW-SE
+            slice_nw_se = [window[0,4],window[1,3],window[2,2],window[3,1],window[4,0]]
+            # NE-SW
+            slice_ne_sw = [window[0,0],window[1,1],window[2,2],window[3,3],window[4,4]]
+
+            all_min = [nanmin(slice_w_e), nanmin(slice_n_s),
+                       nanmin(slice_nw_se), nanmin(slice_ne_sw)]
+            all_max = [nanmax(slice_w_e), nanmax(slice_n_s),
+                       nanmax(slice_nw_se), nanmax(slice_ne_sw)]
+            
+            peak_min = all(peak == center for peak in all_min)
+            peak_max = all(peak == center for peak in all_max)
+
+            if peak_min | peak_max:
+                peak5[i,j] = 1
+
+
+    return peak5
