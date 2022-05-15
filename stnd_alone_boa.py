37ec0e51d26b17f3a17c5e86ff39bef74d7f2657
AlxndrLhr <90059220+AlxndrLhr@users.noreply.github.com>
2022-05-16 10:30:12 +1200

    Update stnd_alone_boa.py
    indexes and minor changes

--- stnd_alone_boa.py
+++ stnd_alone_boa.py
@@ -4,7 +4,7 @@ Created on Wed May  4 12:35:19 2022
 
 @author: alhe551
 
-To execute this file, the files boa_peak3.py, boa_peak5.py and boa_m3in5.py are necessary. A sample file is available.
+To execute this file, the files boa_*.py or pyBOA.py are necessary. A sample file is available.
 """
 
 #Environment setting
@@ -14,7 +14,6 @@ from warnings import simplefilter
 import numpy as np
 import xarray as xr
 from math import sqrt
-from scipy.ndimage import sobel
 from tqdm import tqdm
 from time import sleep
 
@@ -28,10 +27,10 @@ simplefilter("ignore", category=RuntimeWarning)
 #%% Variables and functions
 print('\nLoading custom functions ...')
 
-from boa_mf3in5 import mf3in5
-from boa_peak3 import peak_3
-from boa_peak5 import peak_5
-from boa_sobel_haversine import sobel_haversine
+#from boa_mf3in5 import mf3in5
+#from boa_peak3 import peak_3
+#from boa_peak5 import peak_5
+#from boa_sobel_haversine import sobel_haversine
 
 #or
 
@@ -41,7 +40,7 @@ from pyBOA import peak_5, peak_3, mf3in5, sobel_haversine
 print('\nExtracting nc file ...')
 
 nc_file = 'CHL_4km_sample.nc' 
-nc = xr.open_dataset(nc_file).load()
+nc = xr.open_dataarray(nc_file).load()
 
 # Isolating the log(CHL)
 nc = np.log(nc)
@@ -67,25 +66,25 @@ for day in tqdm(nc.time.values):
         delta_nc = np.subtract(res_fltrd ,CHLa) # differences projected vs measures
         rmse = sqrt(np.nansum(delta_nc**2)/np.nansum(delta_nc/delta_nc)) # RMSE
         print(f' RMSE = {rmse}') # print intermediary RMSE, uncomment to see
-        CHLa = np.copy(res_fltrd) # Reafecting 
+        CHLa = res_fltrd.copy() # Reafecting 
         #rmse = 0 # Security for one run only
     sleep(.2) #aesthetic with tqdm if intermediary RMSE
     #print(f'\n RMSE = {rmse}') # print final RMSE, uncomment to see
 
     # Custom sobel
-    res_sobel = sobel_haversine2(res_fltrd)
+    res_sobel = sobel_haversine(res_fltrd)
 
     # Saving
     if day == nc.time.values.min():
         nc_fltrd = res_fltrd
         nc_peak5 = res_peak5
         nc_peak3 = res_peak3
-        nc_sobel = sobel_grd
+        nc_sobel = res_sobel
     else:
         nc_fltrd = xr.concat([nc_fltrd, res_fltrd], dim='time')
         nc_peak5 = xr.concat([nc_peak5, res_peak5], dim='time')
         nc_peak3 = xr.concat([nc_peak3, res_peak3], dim='time')
-        nc_sobel = xr.concat([nc_sobel, sobel_grd], dim='time')
+        nc_sobel = xr.concat([nc_sobel, res_sobel], dim='time')
 
 # Coupling to original nc for save and display purposes
 #nc['fltrd'] = (['time', 'lat', 'lon'], nc_fltrd.data)
